a22eef298467e8a072d04e043d86ab9244796d90
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-08-04 11:13:06 -0300

    feat: valida informacoes do login e retorna um token de acesso

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,6 +1,11 @@
 import connection from '../databases/postgres.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+// routes process and validations functions
 async function isUserAlredyRegistered(email, name) {
   const { rows: emailRegistered } = await connection.query(
     `SELECT * FROM users WHERE email = $1`,
@@ -17,6 +22,38 @@ async function isUserAlredyRegistered(email, name) {
   if(nameRegistered[0]) return 'nome já registrado';
 }
 
+async function isValidLogin(email, password) {
+  const { rows: userData } = await connection.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [ email ]
+  );
+
+  if(!userData[0]) return false;
+
+  const validPassword = bcrypt.compareSync(password, userData[0].password);
+
+  if(!validPassword) return false;
+
+  return userData;
+}
+
+async function registerToken(userData) {
+  await connection.query(
+    `INSERT INTO sessions ("userId") VALUES ($1)`,
+    [ userData[0].id ]
+  );
+
+  const { rows:userSession } = await connection.query(
+    `SELECT * FROM sessions WHERE "userId" = $1`,
+    [ userData[0].id ]
+  );
+
+  const token = jwt.sign(userSession[0].id, process.env.PRIVATE_KEY_JWT);
+
+  return token;
+}
+
+// routes functions
 export async function setNewUser(req, res) {
   const { name, email, password} = req.body;
 
@@ -44,8 +81,16 @@ export async function setNewUser(req, res) {
 }
 
 export async function signin(req, res) {
+  const { email, password } = req.body;
+
   try {
-    res.sendStatus(200);
+    const userData = await isValidLogin(email, password);
+
+    if(!userData) return res.sendStatus(401);
+
+    const token = await registerToken(userData);
+
+    res.status(200).send({ token });
   } catch(err) {
     console.log(err);
     res.sendStatus(500);
