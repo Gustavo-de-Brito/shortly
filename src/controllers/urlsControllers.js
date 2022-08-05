f880f8ae9d65f1b434f0b2d9a5eedb2a3288742a
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-08-05 14:23:04 -0300

    feat: adiciona rota de direcionamento das urls encurtadas

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -1,6 +1,17 @@
 import connection from '../databases/postgres.js';
 import { nanoid } from 'nanoid';
 
+async function incrementVisitCounter(urlData) {
+  const incrementCounter = urlData[0].visitCount + 1;
+
+  await connection.query(
+    `UPDATE urls
+    SET "visitCount" = $1
+    WHERE id = $2;`,
+    [ incrementCounter, urlData[0].id ]
+  );
+}
+
 export async function decreaseUrl(req, res) {
   const { userId } = res.locals;
   const { url } = req.body;
@@ -42,6 +53,26 @@ export async function getUrl(req, res) {
     if(!urlData[0]) return res.sendStatus(404);
 
     res.status(200).send(urlData[0]);
+  } catch(err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function directShortUrl(req, res) {
+  const { shortUrl } = req.params;
+
+  try{
+    const { rows: urlData } = await connection.query(
+      `SELECT * FROM urls WHERE "shortUrl" = $1;`,
+      [ shortUrl ]
+    );
+
+    if(!urlData[0]) return res.sendStatus(404);
+
+    await incrementVisitCounter(urlData);
+
+    res.redirect(urlData[0].url);
   } catch(err) {
     console.log(err);
     res.sendStatus(500);
