610bd5bf57bf3367f09aeb1658b6ed445c3e95d3
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-08-05 14:48:42 -0300

    feat: adiciona rota de delete de urls atraves do id

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -1,6 +1,7 @@
 import connection from '../databases/postgres.js';
 import { nanoid } from 'nanoid';
 
+// routes process and validations functions
 async function incrementVisitCounter(urlData) {
   const incrementCounter = urlData[0].visitCount + 1;
 
@@ -12,6 +13,16 @@ async function incrementVisitCounter(urlData) {
   );
 }
 
+async function isExistingUrl(urlId) {
+  const { rows: urlData } = await connection.query(
+    `SELECT * FROM urls WHERE id = $1;`,
+    [ urlId ]
+  );
+
+  return urlData;
+}
+
+// routes functions
 export async function decreaseUrl(req, res) {
   const { userId } = res.locals;
   const { url } = req.body;
@@ -73,6 +84,28 @@ export async function directShortUrl(req, res) {
     await incrementVisitCounter(urlData);
 
     res.redirect(urlData[0].url);
+  } catch(err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function deleteUrl(req, res) {
+  const { id: urlId } = req.params;
+  const { userId } = res.locals;
+
+  try {
+    const urlData = await isExistingUrl(urlId);
+
+    if(!urlData[0]) return res.sendStatus(404);
+    if(urlData[0].userId !== userId) return res.sendStatus(401);
+
+    await connection.query(
+      `DELETE FROM urls WHERE id = $1`,
+      [ urlId ]
+    )
+
+    res.sendStatus(204);
   } catch(err) {
     console.log(err);
     res.sendStatus(500);
