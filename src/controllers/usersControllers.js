3aa4956b1297bab64e3a59d6b6c465ae82670753
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-08-06 19:21:59 -0300

    feat: adiciona rota pra consulta dos usuarios com links mais acessados

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -42,3 +42,26 @@ export async function getUserData(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getRanking(req, res) {
+  try {
+    const { rows: topTenData } = await connection.query(
+      `
+      SELECT users.id, users.name, 
+      COUNT(urls.id) AS "linksCount",
+      SUM(urls."visitCount") AS "visitCount"
+      FROM users
+      JOIN urls
+      ON urls."userId" = users.id
+      GROUP BY users.id
+      ORDER BY "visitCount" DESC
+      LIMIT 10;
+      `
+    );
+
+    res.status(200).send(topTenData);
+  } catch(err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
